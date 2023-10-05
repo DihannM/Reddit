8ccae978859b84ef067316a5034916ee253069b2
Dihann <dihannmalan@gmail.com>
2023-10-05 14:31:24 +1300

    Created Home Feature and Added to App.js to render

---
+++ reddit/src/features/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import Post from '../Post/Post';
+
+const Home = () => {
+    return (
+        <>
+            <Post />
+        </>
+    );
+};
+
+export default Home;
