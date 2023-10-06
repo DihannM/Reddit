3c9beed6397b88606bc88bad3a1b9cc03d533285
Dihann <dihannmalan@gmail.com>
2023-10-06 23:11:06 +1300

    Created Avatar feature

---
+++ reddit/src/features/Avatar/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Avatar.css';
+
+const Avatar = () => {
+  return (
+    <img
+      src="/assets/images/astronaut.png"
+      alt="avatar"
+      className="avatar-profile-image"
+    />
+  );
+};
+
+export default Avatar;
